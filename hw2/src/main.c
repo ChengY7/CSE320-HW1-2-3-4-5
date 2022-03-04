13bd11429f5f8e38f04548592ce0b137ca1db962
Cheng Yang <Yang34@cs.stonybrook.edu>
2022-03-04 18:42:50 -0500

    real final

--- hw2/src/main.c
+++ hw2/src/main.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-extern int original_main(int argc, char **argv);
+extern int original_main(int argc, char *const *argv);
 
-int main(int argc, char **argv) {
+int main(int argc, char *const *argv) {
     original_main(argc, argv);
 }
