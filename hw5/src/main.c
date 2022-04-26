723b499cc12ed8337df3ce8ac5c49ea5dd5e93d0
Cheng Yang <Yang34@cs.stonybrook.edu>
2022-04-26 15:33:44 -0400

    server done

--- hw5/src/main.c
+++ hw5/src/main.c
@@ -1,11 +1,19 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 
 #include "pbx.h"
 #include "server.h"
 #include "debug.h"
 
+#include "csapp.h"
+
+
 static void terminate(int status);
+void sighup_handler(int signal) {
+    printf("123");
+    terminate(EXIT_SUCCESS);
+}
 
 /*
  * "PBX" telephone exchange simulation.
@@ -13,10 +21,22 @@ static void terminate(int status);
  * Usage: pbx <port>
  */
 int main(int argc, char* argv[]){
+    printf("%d\n", PBX_MAX_EXTENSIONS);
     // Option processing should be performed here.
     // Option '-p <port>' is required in order to specify the port number
     // on which the server should listen.
-
+    int option;
+    char *port;
+    while ((option=getopt(argc, argv, "p:"))!=-1) {
+        switch(option) {
+            case 'p':
+                port=optarg;
+                break;
+            default:
+                printf("testing");
+                break;
+        }
+    }
     // Perform required initialization of the PBX module.
     debug("Initializing PBX...");
     pbx = pbx_init();
@@ -27,6 +47,22 @@ int main(int argc, char* argv[]){
     // a SIGHUP handler, so that receipt of SIGHUP will perform a clean
     // shutdown of the server.
 
+    struct sigaction action;
+    action.sa_handler=sighup_handler;
+    sigaction(SIGHUP, &action, NULL);
+
+    int listenfd, *connfd;
+    listenfd=open_listenfd(port);
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    pthread_t tid;
+
+    while(1) {
+        connfd = malloc(sizeof(int));
+        clientlen = sizeof(struct sockaddr_storage);
+        *connfd=Accept(listenfd, (SA*) &clientaddr, &clientlen);
+        pthread_create(&tid, NULL, pbx_client_service, connfd);
+    }
     fprintf(stderr, "You have to finish implementing main() "
 	    "before the PBX server will function.\n");
 
--- hw5/src/server.c
+++ hw5/src/server.c
@@ -7,15 +7,96 @@
 #include "debug.h"
 #include "pbx.h"
 #include "server.h"
+#include "csapp.h"
 
 /*
  * Thread function for the thread that handles interaction with a client TU.
  * This is called after a network connection has been made via the main server
  * thread and a new thread has been created to handle the connection.
  */
-#if 0
+#if 1
 void *pbx_client_service(void *arg) {
-    // TO BE IMPLEMENTED
-    abort();
+    int connfd = *((int*)arg);
+    free(arg);
+    pthread_detach(pthread_self());
+    TU *tu = tu_init(connfd);
+    if(pbx_register(pbx, tu, connfd)==-1) {
+        return NULL;
+        //error to fix ^
+    }
+    int fd = tu_fileno(tu);
+    if(fd==-1)
+        return NULL;
+        //error to fix ^
+    char buffer;
+    int readResult;
+    char *command;
+    int commandlen=0;
+    int firstChar=1;
+    while (1) {
+        command=malloc(sizeof(char));
+        while((readResult=read(fd, &buffer, 1))) {
+            if(firstChar) {
+                if(buffer=='\r') {
+                    readResult=read(fd, &buffer, 1);
+                    if(buffer=='\n')
+                        break;
+                    else {
+                        command[commandlen]='\r';
+                        commandlen++;
+                        firstChar=0;
+                        command = realloc(command, commandlen+1);
+                        command[commandlen]=buffer; 
+                        commandlen++;
+                    }
+                }
+                command[commandlen]=buffer;
+                commandlen++;
+                firstChar=0;
+            } 
+            else if(buffer=='\r') {
+                readResult=read(fd, &buffer, 1);
+                if (buffer=='\n')
+                    break;
+                else {
+                    command = realloc(command, commandlen+1);
+                    command[commandlen]='\r'; 
+                    commandlen++;
+                    command = realloc(command, commandlen+1);
+                    command[commandlen]=buffer; 
+                    commandlen++;
+                }
+            }
+            else {
+               command = realloc(command, commandlen+1);
+               command[commandlen]=buffer; 
+               commandlen++;
+            }
+        }
+        if(strcmp(command, tu_command_names[TU_PICKUP_CMD])==0) {
+            tu_pickup(tu);
+        }
+        else if(strcmp(command, tu_command_names[TU_HANGUP_CMD])==0) {
+            tu_hangup(tu);
+        }
+        else if(strncmp(command, tu_command_names[TU_DIAL_CMD], 4)==0) {
+            int num = atoi(command+4);
+            if(num>-1 && num<1024) {
+                pbx_dial(pbx, tu, num);
+            }
+        }
+        else if (strncmp(command, tu_command_names[TU_CHAT_CMD], 4)==0) {
+            tu_chat(tu, command+4);
+        }
+        free(command);
+        commandlen=0;
+        firstChar=1;
+    }
+    int unreg = pbx_unregister(pbx, tu);
+    if(unreg==-1)
+        return NULL;
+        //fix ^^
+    close(fd);    
 }
 #endif
+
